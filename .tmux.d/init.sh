7d0b2dec366e1e26424c80ee31e297df5f28160a
jordonbiondo <biondoj@mail.gvsu.edu>
2015-02-27 11:24:34 -0500

    tmux fixes

--- .tmux.d/init.sh
+++ .tmux.d/init.sh
@@ -1,8 +1,16 @@
+source ~/.bashrc
 
 function osx () {
     echo uname | grep -Eho "Darwin"
 }
 
+if [ $(installed reattach-to-user-namespace) ]; then
+    tmux set-option -gq default-command "reattach-to-user-namespace -l bash"
+fi
+
+#
+# OSX specific
+#
 if [ -n $(osx) ]; then
     tmux set-option -gq "status-left" "[#S] #(eval pmset -g batt | grep -Eho '[^-][0-9]+%')%"
 fi
